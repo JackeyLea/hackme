504c1295dc64218533a2f2de32b107d28ce8cc8b
JackeyLea <1768478912@qq.com>
2021-06-29 21:01:50 +0800

    添加时间显示组件

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -6,10 +6,28 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->lcdNumber->setDigitCount(25);//设置显示的位数
+    ui->lcdNumber->setMode(QLCDNumber::Dec);//设置显示模式为十进制
+    ui->lcdNumber->setSegmentStyle(QLCDNumber::Flat);//设置显示外观
+
+    //ui->lcdNumber->setStyleSheet("border: 1px solid green; color: green; background: silver;");//设置样式
+
+    m_pTimer = new QTimer(this);
+    m_pTimer->setInterval(100);//设置时间间隔
+    connect(m_pTimer,&QTimer::timeout,this,&MainWindow::onTimeUpdate);
+    m_pTimer->start();//启动定时器
 }
 
 MainWindow::~MainWindow()
 {
+    delete m_pTimer;
     delete ui;
+}
+
+void MainWindow::onTimeUpdate()
+{
+    QDateTime dateTime = QDateTime::currentDateTime();//获取系统当前时间
+    ui->lcdNumber->display(dateTime.toString("yyyy-MM-dd HH:mm:ss.zzz"));//显示内容，显示格式为年-月-日 时:分:秒.毫秒
 }
 
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -1,7 +1,20 @@
+///
+/// \brief HMLCDNumber
+/// \author JackeyLea
+/// \copyright MIT
+/// \date 2021-03-13
+/// \note 自定义的事件显示模块
+///
+
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLCDNumber>
+#include <QDebug>
+#include <QString>
+#include <QTimer>
+#include <QDateTime>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +28,12 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void onTimeUpdate();
+
 private:
     Ui::MainWindow *ui;
+
+    QTimer *m_pTimer;
 };
 #endif // MAINWINDOW_H
